ae3af40cfd7e2209f330adafbb24d74d7dfa075e
Priyanshu-Prime <pmedu718@gmail.com>
2025-02-07 22:30:45 +0530

    add review joi validation code

--- schema.js
+++ schema.js
@@ -18,4 +18,15 @@ module.exports.listingSchema = joi.object(
             }
         ).required()
     }
+);
+
+module.exports.reviewSchema = joi.object(
+    {
+        review : joi.object(
+            {
+                rating: joi.number.required(),
+                comment: joi.string.required()
+            }
+        ).required()
+    }
 )
