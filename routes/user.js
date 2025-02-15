235520310f9c7c103d9bbd30ae4cb1b937af2df0
Priyanshu-Prime <pmedu718@gmail.com>
2025-02-15 18:46:30 +0530

    complete signup functionality

--- routes/user.js
+++ routes/user.js
@@ -2,10 +2,29 @@ const express = require("express");
 const router = express.Router({mergeParams: true});
 const wrapAsync = require("../utils/wrapAsync.js");
 const ExpressError = require("../utils/ExpressError.js");
+const User = require("../models/user.js");
 
 router.get("/signup", wrapAsync(async(req, res) =>
 {
     res.render("../views/users/signup.ejs");
 }));
+
+router.post("/signup", wrapAsync(async(req, res) =>
+{
+    try
+    {
+        const { username, email, password } = req.body;
+        const user = new User({ username, email });
+        const regUser = await User.register(user, password);
+        console.log(regUser);
+        req.flash("success", `Welcome Asboard ${username}!`)
+        res.redirect("/listings");
+    }
+    catch(err)
+    {
+        req.flash("error", err.message);
+        res.redirect("/signup");
+    }
+}))
 
 module.exports = router;
